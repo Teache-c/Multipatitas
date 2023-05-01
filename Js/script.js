ef09c16939a4d0e00da85c2ecc063279dd72c926
Teache-c <matiasatoledom567@gmail.com>
2023-04-30 20:31:27 -0400

    Revert "UPDATE"
    This reverts commit d33dc11c65ef88fe56e9a1d21310de781161f58f.

--- Js/script.js
+++ Js/script.js
@@ -1,34 +1,18 @@
 $(document).ready(function(){
-    /*Funcion para obtener productos */
+
     $(function () {
         //metodo get para el servicio web y una variable data para guardar el JSON
-        $.get("https://fakestoreapi.com/products", function (data) {
+        $.get("https://www.themealdb.com/api/json/v1/1/categories.php", function (data) {
             //ciclo for para recorrer el JSON y guarda cada elemento de la lista en la variable item
-            $.each(data, function (i, item) {
+            $.each(data.categories, function (i, item) {
                 $("#producto").append(
                     "<div class='dim-carta fon-card'>"+
-                        "<a href='../productos/producto.html' class='nav-link'>"+
-                            "<img src='" + item.image + "' class='img-fluid borde-img' alt='Sin foto'>" +
-                            "<h3 class='px-3 my-3'>" + item.title + "</h6>" +
-                            "<p class='px-3 my-3'>" + item.description + "</p>" +
-                            "<h3 class='px-3 my-3 text-justify'>$"+ item.price +"</h3>" 
-                        +"</a>"
-                    +"</div>"
-                );
-            });
-        });
-    });
-    /* Funcion para obtener un producto */
-    $(function () {
-        //metodo get para el servicio web y una variable data para guardar el JSON
-        $.get("https://fakestoreapi.com/products/1", function (data) {
-            //ciclo for para recorrer el JSON y guarda cada elemento de la lista en la variable item
-            $.each(data, function (i, item) {
-                $("#img-producto").append(
-                    "<img src='" + item.image + "' class='img-fluid borde-img' alt='Sin foto'>"
-                );
-                $("#info-producto").append(
-                    ""
+                    "<a href='../productos/producto.html' class='nav-link con-fx'>"+
+                    "<img src='" + item.strCategoryThumb + "' class='img-fluid borde-img' alt='Sin foto'>" +
+                    "<h6 class='px-3 my-3'>" + item.idCategory + "</h6>" +
+                    "<h6 class='px-3 my-3'>" + item.strCategoryDescription + "</h6>" +
+                    "<h3 class='px-3 my-3'>" + item.strCategory + "</h3>" 
+                    +"</a>"+"</div>"
                 );
             });
         });
