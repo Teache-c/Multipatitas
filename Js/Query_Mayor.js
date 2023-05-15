3bb1fc55f58e0c60b50f0928a4af2954a90dd7ac
Teache-c <matiasatoledom567@gmail.com>
2023-05-15 00:43:07 -0400

    Mod_Perfil Validaciones
    Ya se encuentran implementadas las validaciones de modificar perfil a excepción de la fecha de nacimiento

--- Js/Query_Mayor.js
+++ Js/Query_Mayor.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
     }
     /* Validacion: Caracter Especial */
     if (!special.test(llaves)) {
-      msg += "<p class='text-muted'>" + "La contraseña debe contar con almenos un caracter especial y puedes usar !@#$&*" + "</p>";
+      msg += "<p class='text-muted'>" + "La contraseña debe contar con al menos un caracter especial y puedes usar !@#$&*" + "</p>";
       enviar = true
     }
     var letra = llaves.charAt(0);
@@ -52,7 +52,6 @@ $(document).ready(function () {
       $("#warning").html("Ahora estás en nuestros registros °-°...")
     }
   });
-
   function isMayus(letra) {
     console.log("letra inicial:", letra);
     console.log("Nombre Valido");
@@ -65,101 +64,7 @@ $(document).ready(function () {
     }
   };
 });
-$(function () {
-  $("#trash-btn1").click(function () {
-    $(".prod1").fadeOut();
-  });
-  $("#trash-btn2").click(function () {
-    $(".prod2").fadeOut();
-  });
-  $("#trash-btn3").click(function () {
-    $(".prod3").fadeOut();
-  });
-  $("#trash-btn4").click(function () {
-    $(".prod4").fadeOut();
-  });
-  $("#trash-btn5").click(function () {
-    $(".prod5").fadeOut();
-  });
-  $("#trash-btn6").click(function () {
-    $(".prod6").fadeOut();
-  });
-  $("#trash-btn7").click(function () {
-    $(".prod7").fadeOut();
-  });
-  $("#trash-btn8").click(function () {
-    $(".prod8").fadeOut();
-  });
-});
 
-
-$(function () {
-
-  $(".text1").mouseenter(function () {
-    $("#text1").slideDown();
-  });
-
-  $(".text1").mouseleave(function () {
-    $("#text1").slideUp();
-  });
-
-  $(".text2").mouseenter(function () {
-    $("#text2").slideDown();
-  });
-
-  $(".text2").mouseleave(function () {
-    $("#text2").slideUp();
-  });
-
-  $(".text3").mouseenter(function () {
-    $("#text3").slideDown();
-  });
-
-  $(".text3").mouseleave(function () {
-    $("#text3").slideUp();
-  });
-
-  $(".text4").mouseenter(function () {
-    $("#text4").slideDown();
-  });
-
-  $(".text4").mouseleave(function () {
-    $("#text4").slideUp();
-  });
-
-  $(".text5").mouseenter(function () {
-    $("#text5").slideDown();
-  });
-
-  $(".text5").mouseleave(function () {
-    $("#text5").slideUp();
-  });
-
-  $(".text6").mouseenter(function () {
-    $("#text6").slideDown();
-  });
-
-  $(".text6").mouseleave(function () {
-    $("#text6").slideUp();
-  });
-
-  $(".text7").mouseenter(function () {
-    $("#text7").slideDown();
-  });
-
-  $(".text7").mouseleave(function () {
-    $("#text7").slideUp();
-  });
-
-  $(".text8").mouseenter(function () {
-    $("#text8").slideDown();
-  });
-
-  $(".text8").mouseleave(function () {
-    $("#text8").slideUp();
-  });
-
-});
 
 
 
